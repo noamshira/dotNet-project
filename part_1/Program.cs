145edea4ba15c11dc3cc521d87b1b984359ac2ff
Noam Shira <noamisrael10@gmail.com>
2019-11-08 11:19:18 +0200

    commit

---
+++ part_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random(DateTime.Now.Millisecond); // Define a random variable
+            int[] A = new int[20];
+            int[] B = new int[20];
+            int[] C = new int[20];
+            int size = A.Length;
+            for (int i=0; i<size; i++) // Fills the arrays with numbers
+            {
+                A[i] = r.Next(18, 123);
+                B[i] = r.Next(18, 123);
+            }
+            for (int i = 0; i < size; i++) // Check which array is larger and the difference is put in the array C
+            {
+                if (A[i] > B[i])
+                    C[i] = A[i] - B[i];
+                else C[i] = B[i] - A[i];
+            }
+            for (int i = 0; i < size; i++) //Printing array A
+                Console.Write("{0,-5}",A[i]);
+            Console.WriteLine();
+            for (int i = 0; i < size; i++) //Printing array B
+                Console.Write("{0,-5}", B[i]);
+            Console.WriteLine();
+            for (int i = 0; i < size; i++)
+                Console.Write("{0,-5}", C[i]); //Printing array C
+            Console.WriteLine();
+
+        }
+
+    }
+}
---
+++ part_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] hotel = new bool[12, 31];
+            for (int i = 0; i < 12; i++)
+                for (int j = 0; j < 31; j++)
+                    hotel[i, j] = false;
+            int choice = 0;
+            while (choice!=4)
+
+
+
+
+        }
+    }
+}
